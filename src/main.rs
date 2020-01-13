8a54a0da80b6b1c7718fc403e222f0c7127b15b4
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-12 22:55:40 -0800

    main: add dummy memory dump

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use chirp::memory;
+
+use std::convert::TryFrom;
+
 fn main() {
+    let memory = memory::Memory::default();
+    memory.dump();
+    let memory = memory::Memory::try_from("../games/tetris.ch8").unwrap();
+    memory.dump();
+
     println!("Chirp!")
 }
