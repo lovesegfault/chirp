d4beb023cc0531c19abdc8a0bf6166e18cd9f505
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-13 21:31:03 -0800

    instructions: add top level table

--- src/instructions.rs
+++ src/instructions.rs
@@ -7,10 +7,54 @@
 /// sprite data, it should be padded so any instructions following it will be properly situated in
 /// RAM.
 ///
-/// | Symbol | Width (bits)  | Addressing Mode |
-/// |:------:|:-------------:|:---------------:|
-/// | X, Y   | 4             | Register        |
-/// | k...   | 4 * k         | Immediate       |
+/// | Symbol   | Width (bits) | Addressing Mode |
+/// |:--------:|:------------:|:---------------:|
+/// | `x`, `y` | 4            | Register        |
+/// | `k`...   | 4 * `k`      | Immediate       |
+///
+/// | OpCode   | ASM                  | Op                                                                        |
+/// | -------- | -------------------- | ------------------------------------------------------------------------- |
+/// | `0kkk`   | `SYS addr`           | Jump to a machine code routine at `kkk` [DEPRECATED]                      |
+/// | `00Ck`   | `SCD`                | Scroll down `k` lines                                                     |
+/// | `00FB`   | `SCR`                | Scroll right by 4 pixel                                                   |
+/// | `00FC`   | `SCL`                | Scroll left by 4 pixel                                                    |
+/// | `00FD`   | `EXIT`               | Quit the emulator                                                         |
+/// | `00FE`   | `LOW`                | Set CHIP-8 graphics mode                                                  |
+/// | `00FF`   | `HIGH`               | Set SCHIP-8 graphics mode                                                 |
+/// | `00E0`   | `CLS`                | Clear the display                                                         |
+/// | `00EE`   | `RET`                | Return from a subroutine                                                  |
+/// | `1kkk`   | `JP addr`            | Jump to location `kkk`                                                    |
+/// | `2kkk`   | `CALL addr`          | Call subroutine at `kkk`                                                  |
+/// | `3xkk`   | `SE Vx, byte`        | Skip next instruction if `Vx = kk`                                        |
+/// | `4xkk`   | `SNE Vx, byte`       | Skip next instruction if `Vx != kk`                                       |
+/// | `5xy0`   | `SE Vx, Vy`          | Skip next instruction if `Vx = Vy`                                        |
+/// | `6xkk`   | `LD Vx, byte`        | Set Vx = kk                                                               |
+/// | `7xkk`   | `ADD Vx, byte`       | Set Vx = Vx + kk                                                          |
+/// | `8xy0`   | `LD Vx Vy`           | Set Vx = Vy                                                               |
+/// | `8xy1`   | `OR Vx, Vy`          | Set Vx = Vx OR Vy                                                         |
+/// | `8xy2`   | `AND Vx, Vy`         | Set Vx = Vx AND Vy                                                        |
+/// | `8xy3`   | `XOR Vx, Vy`         | Set Vx = Vx XOR Vy                                                        |
+/// | `8xy4`   | `ADD Vx, Vy`         | Set Vx = Vx + Vy, VF = carry                                              |
+/// | `8xy5`   | `SUB Vx, Vy`         | Set Vx = Vx - Vy, VF = not borrow                                         |
+/// | `8xy6`   | `SHR Vx{, Vy}`       | Set Vx = Vx SHR 1, VF = carry                                             |
+/// | `8xy7`   | `SUBN Vx{, Vy}`      | Set Vx = Vy - Vx, set VF = not borrow                                     |
+/// | `8xyE`   | `SHL Vx{, Vy}`       | Set Vx = Vx SHL 1, VF = carry                                             |
+/// | `9xy0`   | `SNE Vx, Vy`         | Skip next instruction if Vx != Vy                                         |
+/// | `Akkk`   | `LD I, addr`         | Set I = kkk                                                               |
+/// | `Bkkk`   | `JP V0, addr`        | Jump to location kkk + V0                                                 |
+/// | `Cxkk`   | `RND Vx, byte`       | Set Vx = random byte AND kk                                               |
+/// | `Dxyk`   | `DRW Vx, Vy, nibble` | Display n-byte sprite starting at M[I] from (Vx, Vy), set VF = collision  |
+/// | `Ex9E`   | `SKP Vx`             | Skip next instruction if key with the value of Vx is pressed.             |
+/// | `ExA1`   | `SNKP Vx`            | Skip next instruction if key with the value of Vx is not pressed          |
+/// | `Fx07`   | `LD Vx, DT`          | Set Vx = delay timer value                                                |
+/// | `Fx0A`   | `LD Vx, K`           | Wait for a key press, store the value of the key in Vx                    |
+/// | `Fx15`   | `LD DT, Vx`          | Set delay timer = Vx                                                      |
+/// | `Fx18`   | `LD ST, Vx`          | Set sound timer = Vx                                                      |
+/// | `Fx1E`   | `ADD I, Vx`          | Set I = I + Vx                                                            |
+/// | `Fx29`   | `LD F, Vx`           | Set I = location of sprite for digit Vx                                   |
+/// | `Fx33`   | `LD B, Vx`           | Store BCD representation of Vx in memory locations I, I+1, and I+2        |
+/// | `Fx55`   | `LD [I], Vx`         | Store registers V0 through Vx in memory starting at location I            |
+/// | `Fx65`   | `LD Vx, [I]`         | Read registers V0 through Vx from memory starting at location I           |
 pub enum Instruction {
     /// | OpCode   | ASM                  | Op                                                                        |
     /// | -------- | -------------------- | ------------------------------------------------------------------------- |
